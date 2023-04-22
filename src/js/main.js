d56cee1721085b51d940168c82fdea09abb55c2b
Rafał <rafal.klocek.praca@gmail.com>
2023-04-22 13:20:31 +0200

    added photos

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,19 @@
-const test = () => {
-    console.log("witam");
-    // komentarz
+const mobileBtn = document.querySelector(".nav_hamburger");
+const mobileNav = document.querySelector(".nav_hamburger-listbox");
+const mobileLink = document.querySelectorAll(
+	".nav_hamburger-listbox-list-item-link"
+);
+
+const showMobileNav = () => {
+	mobileNav.classList.toggle("active");
+    document.body.classList.add("hidden-body")
+	
+    mobileLink.forEach(link => {
+        link.addEventListener("click", () => {
+            mobileNav.classList.remove("active");
+            document.body.classList.remove("hidden-body")
+        })
+    })
 };
+
+mobileBtn.addEventListener("click", showMobileNav);
