8136399514cb1c7370e5afc89a051f3ecb7e8792
Rafał <rafal.klocek.praca@gmail.com>
2023-04-24 20:38:35 +0200

    site without scrollspy

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,18 @@ const mobileLink = document.querySelectorAll(
 	".nav-mobile_hamburger-list-item-link"
 );
 const footerYear = document.querySelector(".footer-text-span")
+const nav = document.querySelector(".nav")
 
 const showMobileNav = () => {
 	mobileNav.classList.toggle("active");
 	document.body.classList.toggle("hidden-body");
+	mobileBtn.classList.toggle("is-active")
 
 	mobileLink.forEach((link) => {
 		link.addEventListener("click", () => {
 			mobileNav.classList.remove("active");
 			document.body.classList.remove("hidden-body");
+			mobileBtn.classList.remove("is-active")
 		});
 	});
 };
@@ -22,5 +25,15 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year;
 };
 
+const changeColor = () => {
+	if(scrollY > 5) {
+		nav.classList.add("dark")
+	} else {
+		nav.classList.remove("dark")
+	}
+}
+
 handleCurrentYear();
 mobileBtn.addEventListener("click", showMobileNav);
+window.document.addEventListener("scroll", changeColor)
+
