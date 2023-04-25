d80f3f746c39214a692811f39e97fca9116646c1
Rafał <rafal.klocek.praca@gmail.com>
2023-04-25 16:33:58 +0200

    site done

---
+++ README.md
@@ -0,0 +1,13 @@
+# Forest 
+
+**Used Languages/Technologies:**
+
+1. HTML
+2. CSS
+3. SCCS
+4. JS
+
+***Fully working site about forest. Fully responsive.***
+
+
+Link to [site](https://ludzikk.github.io/Forest-Website/), it was built using [GitHub Pages](https://pages.github.com/).
--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,12 @@ const mobileNav = document.querySelector(".nav-mobile");
 const mobileLink = document.querySelectorAll(
 	".nav-mobile_hamburger-list-item-link"
 );
+
 const footerYear = document.querySelector(".footer-text-span")
+
 const nav = document.querySelector(".nav")
+const section = document.querySelectorAll(".scrollspy")
+const navLinks = document.querySelectorAll(".nav_list-item-link")
 
 const showMobileNav = () => {
 	mobileNav.classList.toggle("active");
@@ -31,6 +35,24 @@ const changeColor = () => {
 	} else {
 		nav.classList.remove("dark")
 	}
+}
+
+window.onscroll = () => {
+	section.forEach(sec => {
+		let top = window.scrollY;
+		let offset = sec.offsetTop;
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute("id")
+
+		if(top >= offset && top < offset + height ) {
+			navLinks.forEach(links => {
+				links.parentElement.classList.remove("scrollspy-active")
+				links.classList.remove("scrollspy-active-item")
+				document.querySelector(".nav_list-item-link[href*=" + id + "]").parentElement.classList.add("scrollspy-active")
+				document.querySelector(".nav_list-item-link[href*=" + id + "]").classList.add("scrollspy-active-item")
+			})
+		}
+	})
 }
 
 handleCurrentYear();
